136b813f89178e8af639eaff0196479effb925e4
jihyeryu <bonnie.jr54@gmail.com>
2020-08-15 03:08:01 +0900

    1431.  Kids With the Greatest Number of Candies

---
+++ 1431.js
@@ -0,0 +1,25 @@
+
+//  @param {number[]} candies
+//  @param {number} extraCandies
+//  @return {boolean[]}
+ 
+const kidsWithCandies = function(candies, extraCandies) {
+    
+    let arr = []
+  
+    for(let i = 0 ; i<candies.length ; i++){
+
+       let a = extraCandies + candies[i]
+
+        if (a >=  Math.max.apply(null,candies)){
+      
+            arr.push(true)
+        } else {
+            arr.push(false) 
+        }
+         
+    }
+    return arr
+};
+
+kidsWithCandies([2,3,5,1,3],3) //[true,true,true,false,true] 
