ece2e0977c95e8b823154c51ec12940c252aa9f1
jihyeryu <bonnie.jr54@gmail.com>
2020-08-17 18:36:46 +0900

    1470

---
+++ 1470.js
@@ -0,0 +1,20 @@
+/*
+ @param {number[]} nums
+ @param {number} n
+ @return {number[]}
+ */
+
+ let arr = []
+
+ const shuffle = function(nums, n) {
+    
+    for(let i = 0;i<n;i++){
+            return nums[i], nums[i+n]
+            
+        
+        }
+     
+    return arr
+};
+
+console.log([2,5,1,3,4,7],3);
