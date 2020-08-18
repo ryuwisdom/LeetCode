1a63da7bba044af36b4411ca4fcfe4ca726f462d
jihyeryu <bonnie.jr54@gmail.com>
2020-08-18 22:21:42 +0900

    1512_complted

---
+++ 1512.js
@@ -0,0 +1,15 @@
+
+const numIdenticalPairs = function(nums) {
+    let result = []
+
+    for(let i = 0 ; i<nums.length ; i++) {
+        for(let j = i ; j<nums.length ; j++) {
+            if(nums[i]==nums[j] && i<j){
+                result.push([i,j])
+            }
+        }
+
+    }
+    return result.length
+};
+console.log(numIdenticalPairs([1,2,3,1,1,3]));
