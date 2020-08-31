afdace92d6c35bc2fac85f9f60e8757e3c7f4621
jihyeryu <bonnie.jr54@gmail.com>
2020-08-31 13:42:13 +0900

    completed 1528

--- 1528.js
+++ 1528.js
@@ -1,18 +1,28 @@
+// const restoreString = function(s, indices) {
+//   let answer = {}
+// for(let i = 0;i<s.length;i++){
 
-const restoreString = function(s, indices) {
-    
-let i = 0
-let map = new Map();
-for(let i = 0;i<s.length;i++){
+//   answer[indices[i]] = s[i]
 
-    
-    map.set(s[i], indices[i])
-    
-    
+// }
+
+// const submit = Object.values(answer)
+// console.log(submit.join(''));
+// return submit.join('')
+// };
+
+// console.log(restoreString("codeleet", [4,5,6,7,0,2,1,3])); //"leetcode"
+
+const arr = [1, 2, 3, 4, 5];
+
+const callback = function (a){
+  return a*10
 }
-console.log([indices[i] , s[i]]);
+// arr.map(a)
+console.log(arr.map(callback));
 
 
-
-};
-console.log(restoreString("codeleet", [4,5,6,7,0,2,1,3])); //"leetcode"
+// map 함수는 
+// 파라미터로 받은 함수를
+// .앞에있는 배열의 모든 요소에
+// 적용시킨다
